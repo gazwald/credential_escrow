a0097feb65924ecc3ba7a9e32abf4167e67954ad
gazwald <gary.brandon@gmail.com>
2020-08-01 11:20:22 +1000

    Basic OTP generator

--- otp-lambda/app.py
+++ otp-lambda/app.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
+import random
+import string
+import json
+
+def generate_character_set():
+    return string.ascii_lowercase + string.ascii_uppercase + string.digits
+
+def generate_otp(length=32):
+    characters = generate_character_set()
+
+    otp = ''.join(random.choices(characters, k=length))
+
+    return otp
 
 def handler(event, context):
-    pass
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        'body': json.dumps({'otp': generate_otp()})
+    }
 
 if __name__ == '__main__':
-    handler(None, None)
+    print(handler(None, None))
