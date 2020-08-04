38b54489990f1a563d721eed3e14c2e64b018a25
gazwald <gary.brandon@gmail.com>
2020-08-04 12:49:41 +1000

    Renaming create to set because it's aesthetically pleasing.

--- credential_escrow/credential_escrow_stack.py
+++ credential_escrow/credential_escrow_stack.py
@@ -16,13 +16,13 @@ class CredentialEscrowStack(core.Stack):
         self.api_escrow = self.create_escrow_resource()
 
         # Define Lambdas and their integrations
-        self.lambda_escrow_create = self.create_escrow_create_lambda()
+        self.lambda_escrow_set = self.create_escrow_set_lambda()
         self.lambda_escrow_get = self.create_escrow_get_lambda()
-        self.lambda_escrow_create_integration = self.create_escrow_create_integration()
+        self.lambda_escrow_set_integration = self.create_escrow_set_integration()
         self.lambda_escrow_get_integration = self.create_escrow_get_integration()
 
         # Add Lambda Integrations to API Resources
-        self.add_escrow_create_lambda_to_api()
+        self.add_escrow_set_lambda_to_api()
         self.add_escrow_get_lambda_to_api()
 
     def create_api(self):
@@ -31,28 +31,28 @@ class CredentialEscrowStack(core.Stack):
     def create_escrow_resource(self):
         return self.api.root.add_resource("escrow")
 
-    def create_escrow_create_lambda(self):
-        return aws_lambda.Function(self, "escrow-create-lambda",
-            code=aws_lambda.Code.from_asset(os.path.join(os.getcwd(), "lambda-escrow-create")),
+    def create_escrow_set_lambda(self):
+        return aws_lambda.Function(self, "lambda-escrow-set",
+            code=aws_lambda.Code.from_asset(os.path.join(os.getcwd(), "lambda-escrow-set")),
             handler="app.handler",
             runtime=aws_lambda.Runtime.PYTHON_3_6
         )        
 
     def create_escrow_get_lambda(self):
-        return aws_lambda.Function(self, "escrow-get-lambda",
+        return aws_lambda.Function(self, "lambda-escrow-get",
             code=aws_lambda.Code.from_asset(os.path.join(os.getcwd(), "lambda-escrow-get")),
             handler="app.handler",
             runtime=aws_lambda.Runtime.PYTHON_3_6
         )        
 
-    def create_escrow_create_integration(self):
-        return apigateway.LambdaIntegration(self.lambda_escrow_create)
+    def create_escrow_set_integration(self):
+        return apigateway.LambdaIntegration(self.lambda_escrow_set)
 
     def create_escrow_get_integration(self):
         return apigateway.LambdaIntegration(self.lambda_escrow_get)
 
-    def add_escrow_create_lambda_to_api(self):
-        self.api_escrow.add_method("POST", self.lambda_escrow_create_integration)
+    def add_escrow_set_lambda_to_api(self):
+        self.api_escrow.add_method("POST", self.lambda_escrow_set_integration)
 
     def add_escrow_get_lambda_to_api(self):
         self.api_escrow.add_method("GET", self.lambda_escrow_get_integration)
